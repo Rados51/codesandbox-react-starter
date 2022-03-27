c6e098827f1725d5f130838969ee0d866b38bb8a
Radoš <rados.fb@gmail.com>
2022-03-27 20:21:35 +0200

    R: CC

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import { StrictMode } from "react";
 import { Provider } from "jotai";
 import App from "./App";
-import "@/css/main.scss";
 
 const container = document.getElementById("app");
 const entry = createRoot(container);
